80ba4ef417d7e35e479dbd61fe397d3cb1624550
Brian Gonzalez <brian.gonzalez@geni.com>
2012-08-09 08:46:41 -0700

    updated genicons gh-page

---
+++ Rakefile.rb
@@ -0,0 +1,21 @@
+
+namespace :genicons do
+
+  desc "Compile Jekyll site and copy files to necessary folders"
+  task :compile do
+    %x[rm -rf css && rm -rf js && rm index.html]
+    %x[cd _jekyll_site && jekyll && cd ..]
+    %x[cp -r _jekyll_site/js . && cp -r _jekyll_site/css . && cp _jekyll_site/index.html .]
+  end
+
+  desc "Removes compiled/copied site files"
+  task :clean do
+    %x[rm -rf css && rm -rf js && rm index.html]
+  end
+
+  desc "Starts up a server to view site"
+  task :server do
+    %x[cd _jekyll_site && jekyll --server]
+  end
+
+end
---
+++ _jekyll_site/Rakefile.rb
@@ -0,0 +1,14 @@
+
+namespace :genicons do
+
+  desc "Compile Jekyll site and copy files to necessary folders"
+  task :compile do
+    
+    old_cwd = Dir.cwd
+    %x[]
+
+  end
+
+
+
+end
